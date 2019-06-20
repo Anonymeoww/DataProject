245b19b1f7f6eb969d9b5e44be1bf72be5b2ae66
Anonymeoww <47354277+Anonymeoww@users.noreply.github.com>
2019-06-20 11:41:26 +0200

    added static multiline chart

--- hotstuff.js
+++ hotstuff.js
@@ -1,13 +1,33 @@
-d3v5.json("hotstuff.json").then(function(all_data) {
-    var correct_data = select_data(all_data,2018);
+Promise.all([d3v5.json("hotstuffwgenres.json"), d3v5.json("positions.json")]).then(function(all_data) {
+    var correct_data = select_data(all_data[0],2018);
+    var correct_data2 = all_data[1];
+    console.log(correct_data2);
     create_bubbless(correct_data);
-    create_radar(correct_data, 0)
+    create_radar(correct_data, 0);
+    create_lines(correct_data2);
+
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+        var svgSC = d3v5.select(".scatter");
+        svgSC.selectAll("*").remove();
+        var svgR = d3v5.select(".radar");
+        svgR.selectAll("*").remove();
+        var correct_data = select_data(all_data[0], this.value);
+        create_bubbless(correct_data);
+        create_radar(correct_data, 0);
+    }
 });
 
 function select_data(dataset, t){
+    var int_t = parseInt(t, 10);
     var data_array = [];
-    Object.keys(dataset[t]).forEach(function (list_element) {
-        data_array.push(dataset[t][list_element])
+    Object.keys(dataset[int_t]).forEach(function (list_element) {
+        data_array.push(dataset[int_t][list_element])
     });
     return data_array
 }
@@ -96,7 +116,7 @@ function create_bubbless(dataset){
                       .attr("cy", function (d, i){
                           return yScale(d["AF"][0]["danceability"])
                       })
-                      .attr("r", 3)
+                      .attr("r", 5)
                       .attr("cx", function (d, i){
                           return xScale(d["AF"][0]["energy"])
                       })
@@ -172,14 +192,159 @@ function select_genre() {
 
 }
 
-function create_lines() {
+function create_lines(dataset) {
+    console.log(dataset[2018]["Ariana Grande"]);
+    var data = dataset[2018]["Ariana Grande"];
 
+    var width = 500;
+    var height = 250;
+    var margin = 50;
+    var duration = 250;
+
+    var lineOpacity = "0.25";
+    var lineOpacityHover = "0.85";
+    var otherLinesOpacityHover = "0.1";
+    var lineStroke = "1.5px";
+    var lineStrokeHover = "2.5px";
+
+    var circleOpacity = '0.85';
+    var circleOpacityOnLineHover = "0.25";
+    var circleRadius = 3;
+    var circleRadiusHover = 6;
+
+    /* Scale */
+    var xScale = d3v5.scaleLinear()
+      .domain([0, 52])
+      .range([0, width-margin]);
+
+    var yScale = d3v5.scaleLinear()
+      .domain([0, 100])
+      .range([height-margin, 0]);
+
+    var color = d3v5.scaleOrdinal(d3v5.schemeCategory10);
+
+    /* Add SVG */
+    var svg = d3v5.select(".line").append("svg")
+      .attr("width", (width+margin)+"px")
+      .attr("height", (height+margin)+"px")
+      .append('g')
+      .attr("transform", `translate(${margin}, ${margin})`);
+
+
+    /* Add line into SVG */
+    var line = d3v5.line()
+      .x(d => xScale(d.week))
+      .y(d => yScale(d.pos));
+
+    let lines = svg.append('g')
+      .attr('class', 'lines');
+
+    lines.selectAll('.line-group')
+      .data(data).enter()
+      .append('g')
+      .attr('class', 'line-group')
+      .on("mouseover", function(d, i) {
+          svg.append("text")
+            .attr("class", "title-text")
+            .style("fill", color(i))
+            .text(d.name)
+            .attr("text-anchor", "middle")
+            .attr("x", (width-margin)/2)
+            .attr("y", 5);
+        })
+      .on("mouseout", function(d) {
+          svg.select(".title-text").remove();
+        })
+      .append('path')
+      .attr('class', 'line')
+      .attr('d', d => line(d.values))
+      .style('stroke', (d, i) => color(i))
+      .style('opacity', lineOpacity)
+      .on("mouseover", function(d) {
+          d3v5.selectAll('.line')
+                        .style('opacity', otherLinesOpacityHover);
+          d3v5.selectAll('.circle')
+                        .style('opacity', circleOpacityOnLineHover);
+          d3v5.select(this)
+            .style('opacity', lineOpacityHover)
+            .style("stroke-width", lineStrokeHover)
+            .style("cursor", "pointer");
+        })
+      .on("mouseout", function(d) {
+          d3v5.selectAll(".line")
+                        .style('opacity', lineOpacity);
+          d3v5.selectAll('.circle')
+                        .style('opacity', circleOpacity);
+          d3v5.select(this)
+            .style("stroke-width", lineStroke)
+            .style("cursor", "none");
+        });
+
+
+    /* Add circles in the line */
+    lines.selectAll("circle-group")
+      .data(data).enter()
+      .append("g")
+      .style("fill", (d, i) => color(i))
+      .selectAll("circle")
+      .data(d => d.values).enter()
+      .append("g")
+      .attr("class", "circle")
+      .on("mouseover", function(d) {
+          d3v5.select(this)
+            .style("cursor", "pointer")
+            .append("text")
+            .attr("class", "text")
+            .text(`${d.price}`)
+            .attr("x", d => xScale(d["week"]) + 5)
+            .attr("y", d => yScale(d.pos) - 10);
+        })
+      .on("mouseout", function(d) {
+          d3v5.select(this)
+            .style("cursor", "none")
+            .transition()
+            .duration(duration)
+            .selectAll(".text").remove();
+        })
+      .append("circle")
+      .attr("cx", function(d) { console.log(typeof(d.pos)); xScale(d["week"])})
+      .attr("cy", d => yScale(d.pos))
+      .attr("r", circleRadius)
+      .style('opacity', circleOpacity)
+      .on("mouseover", function(d) {
+            d3v5.select(this)
+              .transition()
+              .duration(duration)
+              .attr("r", circleRadiusHover);
+          })
+        .on("mouseout", function(d) {
+            d3v5.select(this)
+              .transition()
+              .duration(duration)
+              .attr("r", circleRadius);
+          });
+
+
+    /* Add Axis into SVG */
+    var xAxis = d3v5.axisBottom(xScale).ticks(5);
+    var yAxis = d3v5.axisLeft(yScale).ticks(5);
+
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", `translate(0, ${height-margin})`)
+      .call(xAxis);
+
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append('text')
+      .attr("y", 15)
+      .attr("transform", "rotate(-90)")
+      .attr("fill", "#000")
+      .text("Total values");
 }
 
 function create_radar(year_data, index) {
-
-    console.log(year_data);
-
     var dataset = year_data[index]["AF"][0];
     var axes = ["acousticness", "danceability", "energy", "liveness", "valence"];
     var datas = [];
